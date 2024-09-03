8f03fec1ad7e2c7d5cd23d11932b53da2628b1cb
Super-Matter <60855176+Super-Matter@users.noreply.github.com>
2024-09-03 11:23:23 +0300

    changed extrusion temp values

--- installer.sh
+++ installer.sh
@@ -82,11 +82,11 @@ rm -rf /root/.cache
 wget -q --no-check-certificate https://raw.githubusercontent.com/ballaswag/k1-discovery/main/bin/curl -O /tmp/curl
 chmod +x /tmp/curl
 
-ASSET_URL="https://github.com/ballaswag/guppyscreen/releases/latest/download/$ASSET_NAME.tar.gz"
+ASSET_URL="https://github.com/super-matter/guppyscreen/releases/latest/download/$ASSET_NAME.tar.gz"
 
 if [ "$1" = "nightly" ] || [ "$2" = "nightly" ]; then
     printf "${yellow}Installing nightly build ${white}\n"
-    ASSET_URL="https://github.com/ballaswag/guppyscreen/releases/download/nightly/$ASSET_NAME.tar.gz"
+    ASSET_URL="https://github.com/super-matter/guppyscreen/releases/download/nightly/$ASSET_NAME.tar.gz"
 fi
 
 printf "${green} Downloading asset: $ASSET_NAME.tar.gz ${white}\n"
